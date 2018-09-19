d173b88fc613da11513edfbf0e1a624f6d116a91
THD-AW <tufan.doenmez@googlemail.com>
2018-09-19 22:00:03 +0200

    Read/Write auskommentiert Fehler bei manueller Datumsauswahl muss noch behoben werden

--- app/src/main/java/com/example/thd/flexitime/MainActivity.java
+++ app/src/main/java/com/example/thd/flexitime/MainActivity.java
@@ -14,7 +14,14 @@ import android.widget.DatePicker;
 import android.widget.DigitalClock;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -268,4 +275,37 @@ public class MainActivity extends AppCompatActivity {
         btnExit = findViewById(R.id.btn_exit);
 
     }
+    /*
+    public void read(View view) {
+        try {
+            FileInputStream fileInputStream= openFileInput("myText.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuffer stringBuffer = new StringBuffer();
+            String lines;
+            while ((lines=bufferedReader.readLine())!=null) {
+                stringBuffer.append(lines+"\n");
+            }
+            textView.setText(stringBuffer.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void write(View view) {
+        String Mytextmessage  = editText.getText().toString();
+        try {
+            FileOutputStream fileOutputStream = openFileOutput("myText.txt",MODE_PRIVATE);
+            fileOutputStream.write(Mytextmessage.getBytes());
+            fileOutputStream.close();
+            Toast.makeText(getApplicationContext(),"Text Saved",Toast.LENGTH_LONG).show();
+            editText.setText("");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    } */
 }
